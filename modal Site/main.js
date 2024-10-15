4f1d48109d4f766ac550714945df8274101db8d4
ogawa221 <rohimoto7@gmail.com>
2024-10-16 00:30:01 +0900

    Update main.js

--- modal Site/main.js
+++ modal Site/main.js
@@ -1,22 +1,96 @@
-"use strict";
+'use strict';
 
 {
-    const open = document.getElementById("open");
-    const close =document.getElementById("close");
-    const modal = document.getElementById("modal");
-    const mask =document.getElementById("mask");
+  let todos;
 
-    open.addEventListener( "click" , () => {
-        modal.classList.remove("hidden");
-        mask.classList.remove("hidden");
-    });
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
 
-    close.addEventListener( "click" , () => {
-        modal.classList.add("hidden");
-        mask.classList.add("hidden");
+  const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+  
+  const renderTodo = (todo) => {
+    /*
+    - li
+      - label
+        - input
+        - span
+      - button
+    */
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.checked = todo.isCompleted;
+    input.addEventListener('change', () => {
+      todos.forEach((item) => {
+        if (item.id === todo.id) {
+          item.isCompleted = !item.isCompleted;
+        }
+      });
+      saveTodos();
     });
+    const span = document.createElement('span');
+    span.textContent = todo.title;
+    const label = document.createElement('label');
+    label.appendChild(input);
+    label.appendChild(span);
+    const button = document.createElement('button');
+    button.textContent = 'x';
+    button.addEventListener('click', () => {
+      // if (confirm('Sure?') === false) {
+      if (!confirm('Sure?')) {
+          return;
+      }
+      li.remove();
+      todos = todos.filter((item) => {
+        return item.id !== todo.id;
+      });
+      saveTodos();
+    });
+    const li = document.createElement('li');
+    li.appendChild(label);
+    li.appendChild(button);
+    document.querySelector('#todos').appendChild(li);
+  };
 
-    mask.addEventListener("click", () => {
-        close.click();
+  const renderTodos = () => {
+    todos.forEach((todo) => {
+      renderTodo(todo);
     });
+  };
+
+  document.querySelector('#add-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#add-form input');
+    const todo = {
+      id: Date.now(),
+      title: input.value,
+      isCompleted: false,
+    };
+    renderTodo(todo);
+    todos.push(todo);
+    console.table(todos);
+    saveTodos();
+    input.value = '';
+    input.focus();
+  });
+
+  document.querySelector('#purge').addEventListener('click', () => {
+    if (!confirm('Sure?')) {
+      return;
+    }
+    todos = todos.filter((todo) => {
+      return todo.isCompleted === false;
+    });
+    saveTodos();
+    document.querySelectorAll('#todos li').forEach((li) => {
+      li.remove();
+    });
+    renderTodos();
+  });
+
+  renderTodos();
 }
